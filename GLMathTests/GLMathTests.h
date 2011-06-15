8c9533f9ce726726c90e4352c387c9635d9c3a5e
Fjölnir Ásgeirsson <f@ninjakitten.us>
2011-06-15 13:38:33 +0900

    Initial Commit

---
+++ GLMathTests/GLMathTests.h
@@ -0,0 +1,17 @@
+//
+//  GLMathTests.h
+//  GLMathTests
+//
+//  Created by Fjölnir Ásgeirsson on 6/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface GLMathTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ GLMathTests/GLMathTests.m
@@ -0,0 +1,33 @@
+//
+//  GLMathTests.m
+//  GLMathTests
+//
+//  Created by Fjölnir Ásgeirsson on 6/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GLMathTests.h"
+
+
+@implementation GLMathTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GLMathTests");
+}
+
+@end

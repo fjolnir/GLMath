c6f958a01b8cfd1f4f5a6ecbdbeaa40ce137cd51
Fjölnir Ásgeirsson <me@fyolnish.com>
2012-01-13 00:04:33 +0900

    Added non-inlined properly exported wrapper functions

---
+++ GLMathExports.c
@@ -0,0 +1,9 @@
+//
+//  GLMathExports.c
+//  GLMath
+//
+//  Created by Fjölnir Ásgeirsson on 1/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ GLMathExports.h
@@ -0,0 +1,14 @@
+//
+//  GLMathExports.h
+//  GLMath
+//
+//  Created by Fjölnir Ásgeirsson on 1/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef GLMath_GLMathExports_h
+#define GLMath_GLMathExports_h
+
+
+
+#endif
